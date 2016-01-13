ce027fc007b440f6e3a496cb3f939d53fad03bff
Henry Fernández <henry@fierysquirrel.com>
2016-01-13 23:45:58 +0900

    - New documentation (FlashDevelop style)

--- Color.hx
+++ Color.hx
@@ -1,19 +1,23 @@
 package;
 
 /**
- * ...
- * @author Henry D. Fernández B.
- */
+	This class contains values r,b,g for a specified color.
+**/
 class Color 
 {
 	public var r : Float;
 	public var g : Float;
 	public var b : Float;
 	
+	/**
+		Creates a color based on values of `r`, `g`, `b`.
+
+		`r`, `g`, `b` should be a values between 0 and 256.
+	**/
 	public function new(r : Float,g : Float,b : Float) 
 	{
 		this.r = r/255;
 		this.g = g/255;
-		this.b = b/255;
+		this.b = b / 255;
 	}
 }
